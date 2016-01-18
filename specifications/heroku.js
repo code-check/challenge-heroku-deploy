5f35024d942f7cda4b540aef6925f57a4e2f2534
takayukioda <takayuki@odashi.me>
2016-01-19 01:09:40 +0900

    Remove unnecessary codes

--- specifications/heroku.js
+++ specifications/heroku.js
@@ -7,11 +7,6 @@ function origin (appname) {
   return 'https://' + appname + '.herokuapp.com';
 };
 
-function endpoint (appname, path) {
-  return 'https://' + appname + '.herokuapp.com' + (path || '/');
-}
-
 module.exports = {
   origin: origin,
-  endpoint: endpoint
 }
