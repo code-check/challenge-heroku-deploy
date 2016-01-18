39ae4d5a74367f877af978082b7462e2f10d27e2
takayukioda <takayuki@odashi.me>
2016-01-19 01:16:26 +0900

    Update server implementation to match specifications

--- main.js
+++ main.js
@@ -7,9 +7,24 @@ var
 app.use(parser.json());
 
 app.get('/', function (req, res, next) {
+  res.send("Server is Running!!");
+  return next();
+});
+
+app.get('/api/ping', function (req, res, next) {
   res.json('PONG');
   return next();
 });
+
+app.get('/api/notfound', function (req, res, next) {
+  res.status(404).json('NotFound');
+  return next();
+});
+
+app.post('/api/badrequest', function (req, res, next) {
+  res.status(400).json('BadRequest');
+  return next();
+})
 
 app.listen(port, function () {
   console.log('Server running with port', port);
