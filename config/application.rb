6c892719acf3612d5b9799c6aa5de40c8103a43e
takayukioda <takayuki@odashi.me>
2016-01-21 11:44:08 +0900

    Disable asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module ChallengeHerokuDeploy
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Disable pipeline
+    config.assets.enabled = false
   end
 end
