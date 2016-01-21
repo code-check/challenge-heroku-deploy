0f42516599c19b447a1d9a2d23d9845c4bf381da
takayukioda <takayuki@odashi.me>
2016-01-21 20:58:20 +0900

    Initial project

---
+++ src/test/java/jp/co/givery/portfolio/spring/ChallengeApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.co.givery.portfolio.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ChallengeApplication.class)
+public class ChallengeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
