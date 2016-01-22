1d8af4da48d53e19f489773ea6bc6367d405c29b
takayukioda <takayuki@odashi.me>
2016-01-22 15:29:27 +0900

    Implement API

---
+++ src/main/java/jp/co/givery/portfolio/spring/SandboxController.java
@@ -0,0 +1,31 @@
+package jp.co.givery.portfolio.spring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+public class SandboxController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ResponseEntity<String> index () {
+		return new ResponseEntity<String>("\"Server is Running...\"", HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/api/ping", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<String> ping () {
+		return new ResponseEntity<String>("\"PONG\"", HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/api/notfound", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<String> notfound () {
+		return new ResponseEntity<String>("\"NotFound\"", HttpStatus.NOT_FOUND);
+	}
+
+	@RequestMapping(value = "/api/badrequest", method = RequestMethod.POST, produces = "application/json")
+	public ResponseEntity<String> badrequest () {
+		return new ResponseEntity<String>("\"BadRequest\"", HttpStatus.BAD_REQUEST);
+	}
+}
