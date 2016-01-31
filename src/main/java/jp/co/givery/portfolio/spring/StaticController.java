91255db97fa9e0a5c37a488ff52439ed835fabd3
takayukioda <takayuki@odashi.me>
2016-02-01 03:57:33 +0900

    Implement static page
    Spring requires full path to static page, even for index.html.
    Redirect to `/index.html` when accessed to `/`

--- src/main/java/jp/co/givery/portfolio/spring/StaticController.java
+++ src/main/java/jp/co/givery/portfolio/spring/StaticController.java
@@ -1,7 +1,6 @@
 package jp.co.givery.portfolio.spring;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,6 +9,6 @@ public class StaticController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index () {
-		return "index";
+		return "redirect:/index.html";
 	}
 }
